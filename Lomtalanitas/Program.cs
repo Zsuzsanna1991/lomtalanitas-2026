1d8fdddae4d23f19639cc2e8fa57c65b88f7a4c1
Zsuzsanna1991 <zsuzsannaszabo04@gmail.com>
2026-02-18 16:30:27 +0100

    Configure database connection in appsettings and Program.cs

--- Lomtalanitas/Program.cs
+++ Lomtalanitas/Program.cs
@@ -1,24 +1,50 @@
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using Lomtalanitas.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// DbContext regisztrálása
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+    )
+);
 
+// Add services to the container.
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors();
+
+// Statikus fájlok engedélyezése
+app.UseDefaultFiles();   // index.html automatikus betöltése
+app.UseStaticFiles();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage(); // ? mutatja a részletes hibaüzenetet
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
