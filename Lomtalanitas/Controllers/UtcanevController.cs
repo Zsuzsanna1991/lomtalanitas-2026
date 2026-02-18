45ab90301479ea1bb04a0742001b3ed27b0265c1
Zsuzsanna1991 <zsuzsannaszabo04@gmail.com>
2026-02-18 17:47:48 +0100

    Add Data

--- Lomtalanitas/Controllers/UtcanevController.cs
+++ Lomtalanitas/Controllers/UtcanevController.cs
@@ -1,7 +1,7 @@
-﻿using Lomtalanitas.Data;
-using Lomtalanitas.Models;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Lomtalanitas.Data;
+using Lomtalanitas.Models;
 
 namespace Lomtalanitas.Controllers
 {
--- Lomtalanitas/Program.cs
+++ Lomtalanitas/Program.cs
@@ -1,6 +1,6 @@
+using Microsoft.EntityFrameworkCore;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using Lomtalanitas.Data;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
